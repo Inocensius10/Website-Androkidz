8f7626471ddbff94f48e3419aefc4e9923ea66d8
INOCENSIU2203 <inocensiusdwi@gmail.com>
2026-06-05 21:51:25 +0700

    hidden
    ilangin admin

--- src/admin/AdminLogin.jsx
+++ src/admin/AdminLogin.jsx
@@ -44,7 +44,6 @@ function AdminLogin() {
               className="w-full border rounded-lg px-4 py-2 outline-none focus:ring-2 focus:ring-blue-500"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              placeholder="admin123"
             />
           </div>
 
